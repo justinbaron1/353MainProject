62b96cd191684ae42787b4db50b27765bf0cdece
tomleb <tomlebreux@hotmail.com>
2017-12-01 14:18:18 -0500

    Remove old_file from upload if exists

--- utils/upload.php
+++ utils/upload.php
@@ -1,13 +1,22 @@
 <?php
 
+function real_upload_path($upload_dir, $file) {
+  return getcwd() . "/" . $upload_dir . "/" . $file;
+}
+
 // INFO: We're putting the files in a flat way which can cause naming conflicts
 // TODO(tomleb): Handle name conflict, I don't even think it's necessary for the
 // project so I'm skipping this for now.
-function handle_ad_image_upload($mysqli, $file) {
+function handle_ad_image_upload($mysqli, $file, $old_file = '') {
   if ($file['errors'] == UPLOAD_ERR_OK) {
     $ini = parse_ini_file("sikrits.env");
     $upload_dir = $ini["UPLOAD"];
-    $destination_file = getcwd() . "/" . $upload_dir . "/" . $file['name'];
+
+    if ($old_file && $old_file !== '') {
+      unlink(real_upload_path($upload_dir, $old_file));
+    }
+
+    $destination_file = real_upload_path($upload_dir, $file['name']);
 
     // TODO(tomleb): Surround with try catch to prevent warning ?
     $result = move_uploaded_file($file['tmp_name'], $destination_file);
