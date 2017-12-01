e615f7283cd7bd6563bbdfddf19f74717fc49c0f
simon-dube <simon.dube@hotmail.com>
2017-12-01 16:38:36 -0500

    Changed process backup to post

--- trigger-backup.php
+++ trigger-backup.php
@@ -1,12 +1,13 @@
 <?php
     include_once("common/user.php");
 
-    $do = @$_GET["do"];
+    $do = @$_POST["do"];
 
     if(isset($do) && is_admin($mysqli, $user["userId"])){
         $success = do_bills_backup($mysqli);
         if($success){
             $successMessage = "Backup successful!";
+            unset($_GET["do"]);
         }
     }
 
@@ -15,28 +16,29 @@
 <html>
     <head>
         <?php include_once("common/head.php") ?>
-        <script src="js/triggerbackup.js"></script>
     </head>
 
     <body>
-    <?php include("common/navbar.php") ?>
-    <div class="wrapper">
-        <div class="container">
-            <?php if(isset($successMessage)) { ?>
-                <div class="alert alert-success">
-                    <?= $successMessage ?>
+        <?php include("common/navbar.php") ?>
+        <div class="wrapper">
+            <div class="container">
+                <?php if(isset($successMessage)) { ?>
+                    <div class="alert alert-success">
+                        <?= $successMessage ?>
+                    </div>
+                <?php } ?>
+                <h1>Backup Bills</h1>
+                <br/>
+                <p>Welcome to the "Backup Bills" page. By clicking the following button, you will create a backup of the existing transactions.</p>
+                <br/>
+                <div class="text-center">
+                    <form method="post">
+                        <input type="hidden" name="do"/>
+                        <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-save"></span> Process Backup</input>
+                    </form>
                 </div>
-            <?php } ?>
-            <h1>Backup Bills</h1>
-            <br/>
-            <p>Welcome to the "Backup Bills" page. By clicking the following button, you will create a backup of the existing transactions.</p>
-            <br/>
-            <div class="text-center">
-                <a class="btn btn-primary" role="button" href="/trigger-backup?do"><span class="glyphicon glyphicon-save"></span> Process Backup</a>
             </div>
         </div>
-    </div>
-
     </body>
 
 </html>
