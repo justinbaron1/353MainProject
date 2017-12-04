05633f9a77f0bd64d477556fd8c9cb071d19c36e
simon-dube <simon.dube@hotmail.com>
2017-12-03 19:28:22 -0500

    GUID fix

--- post/ad.php
+++ post/ad.php
@@ -106,7 +106,18 @@ function handle_update_ad($ad_id, $user_id, $title, $price, $description,
 
 function name_with_GUID($image_file){
   $path_parts = pathinfo($image_file["name"] );
-  return trim(com_create_guid(), '{}').".".$path_parts["extension"];
+  return guidv4().".".$path_parts["extension"];
+}
+
+function guidv4()
+{
+    if (function_exists('com_create_guid') === true)
+        return trim(com_create_guid(), '{}');
+
+    $data = openssl_random_pseudo_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
 function handle_delete_ad($user_id, $ad_id) {
